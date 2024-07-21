c289856bffbd02dd65ce49285dccf547cff1a35f
Carlo Dandan <carlodandan.personal@proton.me>
2024-07-21 13:26:20 +0000

    Synced with kernel_platform 6
    Signed-off-by: Carlo Dandan <carlodandan.personal@proton.me>

--- prepare_vendor.sh
+++ prepare_vendor.sh
@@ -236,7 +236,7 @@ if [ "${RECOMPILE_KERNEL}" == "1" ]; then
 
   (
     cd ${ROOT_DIR}
-    SKIP_MRPROPER=1 OUT_DIR=${ANDROID_KP_OUT_DIR} ./build/build.sh
+    SKIP_MRPROPER=1 OUT_DIR=${ANDROID_KP_OUT_DIR} ./build.sh
   )
 
   COPY_NEEDED=1
